a1cf1d8305242a4972217fe0b5aa28f9fa049847
Tan Yi Long <120325727+yiiilonggg@users.noreply.github.com>
2023-01-03 21:38:20 +0800

    Create Solution.py

---
+++ 944. Delete Columns to Make Sorted/Solution.py
@@ -0,0 +1,6 @@
+class Solution:
+    def minDeletionSize(self, strs: List[str]) -> int:
+        count = 0
+        for i in zip(*strs):
+            if list(i) != sorted(i): count += 1
+        return count
