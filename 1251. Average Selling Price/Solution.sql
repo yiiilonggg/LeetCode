4fe1c8faa4b431d397c3b9b6079808c90ec0281a
yiiilonggg <120325727+yiiilonggg@users.noreply.github.com>
2024-01-06 16:03:05 +0800

    Solve 1251 (easy)

---
+++ 1251. Average Selling Price/Solution.sql
@@ -0,0 +1,8 @@
+# Write your MySQL query statement below
+SELECT Prices.product_id,
+    IFNULL(ROUND(SUM(Prices.price * UnitsSold.units) / SUM(UnitsSold.units), 2), 0) average_price
+FROM Prices
+LEFT JOIN UnitsSold
+    ON Prices.product_id = UnitsSold.product_id
+        AND UnitsSold.purchase_date BETWEEN Prices.start_date AND Prices.end_date
+GROUP BY Prices.product_id
