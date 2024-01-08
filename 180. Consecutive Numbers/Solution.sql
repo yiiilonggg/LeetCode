1b0c488fb3e1a944010302f314535c93fa863b2b
yiiilonggg <120325727+yiiilonggg@users.noreply.github.com>
2024-01-08 14:27:45 +0800

    Solve 180 (medium)

---
+++ 180. Consecutive Numbers/Solution.sql
@@ -0,0 +1,9 @@
+# Write your MySQL query statement below
+SELECT DISTINCT L1.num ConsecutiveNums
+FROM Logs L1,
+    Logs L2,
+    Logs L3
+WHERE L1.id = L2.id + 1
+    AND L1.id = L3.id + 2
+    AND L1.num = L2.num
+    AND L1.num = L3.num
